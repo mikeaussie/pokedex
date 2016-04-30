fa280ed6de29461f4e47feeb9b46da2b27374b9e
mikeaussie <michael.pyatin@me.com>
2016-04-30 22:41:53 +1000

    added the PokieCell custom class

---
+++ pokedex-by-aurora/PokieCell.swift
@@ -0,0 +1,27 @@
+//
+//  PokieCell.swift
+//  pokedex-by-aurora
+//
+//  Created by Mike Piatin on 30/04/2016.
+//  Copyright © 2016 Aurora Software. All rights reserved.
+//
+
+import UIKit
+
+class PokieCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+    
+    
+    
+}
