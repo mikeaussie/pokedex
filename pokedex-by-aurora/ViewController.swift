05101ded93bc33d859bf28d563a180ccbb1f13f6
mikeaussie <michael.pyatin@me.com>
2016-04-30 17:07:10 +1000

    Initial Commit

---
+++ pokedex-by-aurora/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-aurora
+//
+//  Created by Mike Piatin on 30/04/2016.
+//  Copyright © 2016 Aurora Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
