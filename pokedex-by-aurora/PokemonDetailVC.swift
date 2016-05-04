7762074d507de5bbb2de141ffe1a2cc51ee3e946
mikeaussie <michael.pyatin@me.com>
2016-05-04 18:13:15 +1000

    added outlets in the class and VC

--- pokedex-by-aurora/PokemonDetailVC.swift
+++ pokedex-by-aurora/PokemonDetailVC.swift
@@ -12,16 +12,42 @@ class PokemonDetailVC: UIViewController {
 
     @IBOutlet weak var pokemonLbl: UILabel!
     
+    @IBOutlet weak var mainImg: UIStackView!
+    
+    @IBOutlet weak var descriptionLbl: UIStackView!
+    
+    @IBOutlet weak var typeLbl: UIStackView!
+    
+    @IBOutlet weak var defenceLbl: UIStackView!
+    
+    @IBOutlet weak var heightLbl: UIStackView!
+    
+    @IBOutlet weak var weightLbl: UIStackView!
+    
+    @IBOutlet weak var pokedexIdLbl: UIStackView!
+    
+    @IBOutlet weak var baseAttackLbl: UIStackView!
+    
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    
+    @IBOutlet weak var evoLbl: UILabel!
+    
+    
     var pokemon: Pokemon! //the object to be passed by the segue from another VC
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        pokemonLbl.text = pokemon.name
+        pokemonLbl.text = pokemon.name.capitalizedString
         
         
     }
 
+    @IBAction func goBack(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 
     
 
